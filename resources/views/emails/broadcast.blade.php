8acfd66ad080983fae2241570c655d31ab559288
KLAKKLIK <klakklik.id@gmail.com>
2023-06-22 13:03:03 +0700

    Update broadcast.blade.php

--- resources/views/emails/broadcast.blade.php
+++ resources/views/emails/broadcast.blade.php
@@ -37,23 +37,26 @@
       {{-- <img src="https://halo.yezbozz.id/uploads/logo/1667223693pt-yezbozz-inovasi-digital.png" alt="{{ Library::companyProfile()[0]->company_name }} Logo" width="100" class="img-responsive m-bottom-20"> --}}
           {{-- <h2>Hello, {{ $row['name'] }}</h2> --}}
           <div style="margin-top:20px; margin-bottom:20px">
-            <h5>Harga termurah dan penjual terpercaya hanya di <a href="https://www.tokopedia.com/yezbozz" target="_blank">tokopedia.com/yezbozz</a></h5>
+            <h3>Harga termurah dan penjual terpercaya hanya di <a href="https://www.tokopedia.com/yezbozz" target="_blank">tokopedia.com/yezbozz</a></h3>
             <p>
               {!! $row['message'] !!}
               {{-- <br>Thanks,
               <br>{{ Library::companyProfile()[0]->company_name }} Teams. --}}
-              <h4 style="margin: 10px 0px;">"YEZBOZZ Sahabat Para Pedagang" "Pasti Ada Diskon Diantara KITA"</h4>
-              <h4 style="margin: 10px 0px;">Klik <a href="https://www.tokopedia.com/yezbozz/etalase/discount">disini</a></h4>
+              <h3 style="margin: 10px 0px;">"YEZBOZZ Sahabat Para Pedagang" "Pasti Ada Diskon Diantara KITA"</h3>
+              <h3 style="margin: 10px 0px;">Klik <a href="https://www.tokopedia.com/yezbozz/etalase/discount">disini</a></h3>
               ---------------
-              <h4 style="margin: 10px 0px;">Ikuti Sosial Media Kami, Agar Kamu Dapat Info Harga Sayuran</h4>
-              <h4 style="margin: 10px 0px;">klik link dibawah ini ya. 🔽</h4>
-              <h4><a href="https://www.tokopedia.com/yezbozz" target="_blank">✨Tokped : tokopedia.com/yezbozz</a></h4>
-              <h4><a href="https://www.facebook.com/yezbozz.id" target="_blank">✨Facebook : facebook.com/yezbozz.id</a></h4>
-              <h4><a href="https://www.instagram.com/yezbozz.oss/" target="_blank">✨Instagram : instagram.com/yezbozz.oss/</a></h4>
-              <h4><a href="https://yezbozz.id/" target="_blank">✨Website : yezbozz.id</a></h4>
+              <h3 style="margin: 10px 0px;">Ikuti Sosial Media Kami, Agar Kamu Dapat Info Harga Sayuran TERKINI</h3>
+              <h3 style="margin: 10px 0px;">klik link dibawah ini ya. 🔽</h3>
+              <h3><a href="https://www.tokopedia.com/yezbozz" target="_blank">✨Tokped : tokopedia.com/yezbozz</a></h3>
+              <h3><a href="https://www.facebook.com/yezbozz.id" target="_blank">✨Facebook : facebook.com/yezbozz.id</a></h3>
+              <h3><a href="https://www.instagram.com/yezbozz.oss/" target="_blank">✨Instagram : instagram.com/yezbozz.oss/</a></h3>
+              <h3><a href="https://yezbozz.id/" target="_blank">✨Website : yezbozz.id</a></h3>
               <h3>😍Belanja makin murah makin mudah. Happy
                 shopping kakak😍</h3>
             </p>
+            <div>
+              <a href="https://forms.gle/8Rsu57NwfwPEMifz9" target="_blank" style="color: grey;">Manage your email preferences or unsubscribe</a> 
+            </div>
           </div> 
     </div>
 </body>
